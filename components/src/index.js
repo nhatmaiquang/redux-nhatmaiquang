35f6e303e4f53bc9d6d3cac69721fa671d0b54dd
Mai Quang Nhat <quangnhat224@gmail.com>
2019-02-10 01:27:09 +0200

    some props

--- components/src/index.js
+++ components/src/index.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail
+        author= "Di Me May"
+        timeAgo="Today at 9:00AM"
+        content="Troi oi tao cuoi tao ia"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author= "Cam Lan Suc"
+        timeAgo="Today at 11:00PM"
+        content="con bong long xien"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author= "Chan Mu"
+        timeAgo="Today at 12:00PM"
+        content="Hoa mi hoa mi hot"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
