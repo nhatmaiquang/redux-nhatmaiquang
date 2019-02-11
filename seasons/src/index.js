417564d8722b6bb15182e5dbde4d8a047192bf71
Mai Quang Nhat <quangnhat224@gmail.com>
2019-02-11 18:06:59 +0200

    adding Spinner.js and renderContent() function.

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay'
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
   state = { lat: null, errorMessage: '' };
@@ -12,14 +13,22 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />
     }
-    return <div>Loading!</div>;
+    return <Spinner message = "Dit me accept location di con di cho!" />
+  }
+
+  render() {
+    return (
+      <div className = "border red">
+        {this.renderContent()}
+      </div>
+    );
   }
 }
 
