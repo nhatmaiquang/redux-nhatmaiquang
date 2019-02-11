3656749178533f68058467ec882ff259c484c351
Mai Quang Nhat <quangnhat224@gmail.com>
2019-02-11 16:55:11 +0200

    set seasons state

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  }
+  else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+
+  return (
+    <div>
+      <h1>{season === 'winter' ? 'Lanh vai lon vay du ma' : 'Troi oi nong muon chet'} </h1>
+    </div>
+  );
+};
+
+export default SeasonDisplay;
