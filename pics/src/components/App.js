6f24328aa8d9ac86ddc498ea10a6add59813035f
Mai Quang Nhat <quangnhat224@gmail.com>
2019-02-13 16:24:48 +0200

    create ImageList.js

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
   state = { images: [] };
@@ -19,6 +20,7 @@ class App extends React.Component {
       <div className="ui container" style={{ marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
         Dit me bo may tim thay {this.state.images.length} cai lon
+        <ImageList />
       </div>
     );
   }
