49bffed311f4e11f74291167836f634539d7b894
Mai Quang Nhat <quangnhat224@gmail.com>
2019-02-19 01:33:16 +0200

    updated SearchBar.js

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
   render() {
-    return <div> Cam Lan Suc </div>;
+    return (
+      <div className="ui container">
+        <SearchBar />
+      </div>
+    );
   }
 }
 
