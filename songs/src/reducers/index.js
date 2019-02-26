46b99ec4d6eb4a107f444f51e579ec8cb13cb037
Mai Quang Nhat <quangnhat224@gmail.com>
2019-02-27 00:55:46 +0200

    building reducers

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+  return [
+    { title: 'Cam Lan Suc', duration: '4:05' },
+    { title: 'Phan Kim Tien', duration: '2:30' },
+    { title: 'Tran Mu', duration: '3:49' },
+    { title: 'La Con Bong Long Xien', duration: '5:11' }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
