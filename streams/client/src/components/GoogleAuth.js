86bc80e8df92b8cffb60c4393e2a631dee5fd994
Mai Quang Nhat <quangnhat224@gmail.com>
2019-03-25 19:02:09 +0200

    rendering auth status

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,19 +1,35 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
 
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: '939264010044-2bitp5uis601mamg6d3f1g8imlba2oom.apps.googleusercontent.com',
         scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
       });
     });
   }
 
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>Deo biet la da sign in hay chua</div>;
+    }
+    else if (this.state.isSignedIn) {
+      return <div>Dit me dang nhap roi</div>;
+    }
+    else {
+      return <div>Dit me chua dang nhap</div>;
+    }
+  }
+
   render() {
     return (
-      <div>Google Auth</div>
+      <div>{this.renderAuthButton()}</div>
     );
   }
 }
