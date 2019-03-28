3a728630ee8f7dc976b80ea408548641f7027d4a
Mai Quang Nhat <quangnhat224@gmail.com>
2019-03-28 17:52:54 +0200

    sign in and sign out functions

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -20,13 +20,21 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
     }
     else if (this.state.isSignedIn) {
       return (
-        <button className="ui red google button">
+        <button onClick={this.onSignOut} className="ui red google button">
           <i className="google icon" />
           Đăng xuất má ơi
         </button>
@@ -34,7 +42,7 @@ class GoogleAuth extends React.Component {
     }
     else {
       return (
-        <button className="ui red google button">
+        <button onClick={this.onSignIn} className="ui red google button">
           <i className="google icon" />
           Đăng nhập vô bằng Google má ơi
         </button>
