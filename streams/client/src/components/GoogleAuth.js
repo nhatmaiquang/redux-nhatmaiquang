86b26593f7b88906b953b16e5f98538834e745a9
Mai Quang Nhat <quangnhat224@gmail.com>
2019-03-28 18:27:01 +0200

    onSignIn() => onSignInClick()

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -20,11 +20,11 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
-  onSignIn = () => {
+  onSignInClick = () => {
     this.auth.signIn();
   };
 
-  onSignOut = () => {
+  onSignOutClick = () => {
     this.auth.signOut();
   };
 
@@ -34,7 +34,7 @@ class GoogleAuth extends React.Component {
     }
     else if (this.state.isSignedIn) {
       return (
-        <button onClick={this.onSignOut} className="ui red google button">
+        <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon" />
           Đăng xuất má ơi
         </button>
@@ -42,7 +42,7 @@ class GoogleAuth extends React.Component {
     }
     else {
       return (
-        <button onClick={this.onSignIn} className="ui red google button">
+        <button onClick={this.onSignInClick} className="ui red google button">
           <i className="google icon" />
           Đăng nhập vô bằng Google má ơi
         </button>
